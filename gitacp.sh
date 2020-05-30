37183d3abf51541aa98a06ed080f5164bcd32b94
micaljohn60 <micaljohn60@gmail.com>
2020-05-30 14:18:24 +0630

    add HTML

---
+++ gitacp.sh
@@ -0,0 +1,12 @@
+echo "Enter Commit Message"
+read cmt_msg
+if [$cmt_mst == null];
+then
+echo "Message is Empty! Please add a Commit Message"
+else
+git add --a
+git commit -m"$cmt_msg"
+git push -u origin
+fi
+
+
